23b193f578e3f8379211e14853de2bff3deac8fe
YawoSadji <yawo.sadji@gmx.com>
2024-12-17 12:46:53 -0500

    serializers added

---
+++ api/serializers.py
@@ -0,0 +1,21 @@
+from .models import Product,Cart,User
+from rest_framework import serializers
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('title', 'description', 'price')
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ('total_price', 'user', 'products')
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = () # to be defined
